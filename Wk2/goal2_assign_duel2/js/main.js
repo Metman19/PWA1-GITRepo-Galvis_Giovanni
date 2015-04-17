2490f6db784397d169ec5b895bcbffe26ac66808
Gio <ggalvis@1sale.com>
2015-04-17 00:29:25 -0400

    Project for week two is ready
    Project for week two is ready

---
+++ Wk2/goal2_assign_duel2/js/main.js
@@ -0,0 +1,80 @@
+/**
+ Giovanni Galvis
+ 04/16/2015
+Week #2 :: { Homework } - DEVELOP Duel #2 
+*/
+
+// self-executing function
+(function(){
+
+    console.log("FIGHT!!!");
+
+    //created an array with fighters data.
+    var playerOne = ["Spiderman", 20, 100];
+    var playerTwo = ["Batman",20, 100];
+/*
+    //player name
+    var playerOneName = "Spiderman";    //the variables are blocked out
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+*/
+    //initiate round
+    var round=0;            //replaced all the names, damages, and health's with their place in the array's.
+
+    function fight(){
+        alert(playerOne[0]+":"+playerOne[2]+"  *START*  "+playerTwo[0]+":"+playerTwo[2]);
+        for (var i = 0; i < 10; i++)
+        {
+            //random formula is - Math.floor(Math.random() * (max - min) + min);
+            var minDamage1 = playerOne[1] * .5;
+            var minDamage2 = playerTwo[1] * .5;
+            var f1 = Math.floor(Math.random()*(playerOne[1]-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(playerTwo[1]-minDamage2)+minDamage2);
+
+            //inflict damage
+            playerOne[2]-=f1;
+            playerTwo[2]-=f2;
+
+            console.log(playerOne[0]+": "+playerOne[2] + " " + playerTwo[0]+":"+playerTwo[2]);
+
+            //check for victor
+            var result = winnerCheck();
+            console.log(result);
+            if (result==="no winner")
+            {
+                round++;
+                alert(playerOne[0]+":"+playerOne[2]+"  *ROUND "+round+" OVER"+"*  "+playerTwo[0]+":"+playerTwo[2]);
+
+            } else{
+                alert(result);
+                break;
+            };
+
+          };
+    };
+
+    function winnerCheck(){
+        var result="no winner";
+        if (playerOne[2]<1 && playerTwo[2]<1)
+        {
+            result = "You Both Die";
+        } else if(playerOne[2]<1){
+            result =playerTwo[0]+" WINS!!!"
+        } else if (playerTwo[2]<1)
+        {
+            result = playerOne[0]+" WINS!!!"
+        };
+       return result;
+    };
+
+    /*******  The program gets started below *******/
+    fight();
+
+})();
